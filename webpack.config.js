41cb19735ca437c880a287c3cac405e16a356e6f
Tim Handy <tjhandy@gmail.com>
2016-11-01 18:00:59 +0000

    Webpack now working

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
-let webpack = require('webpack')
+let path = require('path')  // use path here and below in path.join as it's a safe way to use file paths to allow for / or \ depending on which OS the app will run on.
+let HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
 
@@ -6,26 +7,44 @@ module.exports = {
 
     // define entry points (i.e. the first js file that 'requires' other files, this is the one that gets the other .js files started, i.e. links to them)
 
-    entry: './app.js',         // don't forget the comma! It's an object remember.
+    entry: path.join(__dirname, 'src', 'app.js'),         // don't forget the comma! It's an object remember.
 
 
     // define output points
 
     output: {
-        path: 'dist',				// dir will get created automatically. Have also seen this used:  path: __dirname + '/dist',
-        filename: './bundle.js'		// call it whatever you want. The index.html file should have it's script tag for the js pointing to this file. e.g.  <script src="./dist/bundle.js"></script>
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.js'		// call it whatever you want. The index.html file should have it's script tag for the js pointing to this file. e.g.  <script src="./dist/bundle.js"></script>
         // you may see this also called something like scripts.min.js so name it that if you want.
     },
     module: {
         loaders: [
             {
                 test: /\.js$/,
-                exclude: /node_modules/,
-                loader: 'babel',
+                include: path.join(__dirname, 'src'),
+                loader: 'babel-loader',
                 query: {
                     presets: ['es2015']
-                }
+                },
+                plugins: [
+                    'transform-runtime' // the 'transform-runtime' plugin tells babel to require the runtime instead of inlining it.  npm i -D babel-plugin-transform-runtime  and  npm i -S babel-runtime
+                ]
+            },
+            {
+                test: /\.css$/,
+                loader: 'style-loader!css-loader',
+                include: path.join(__dirname, 'src')
             }
         ]
-    }
+    },
+    devServer: {    // npm i -D webpack-dev-server
+        contentBase: path.join(__dirname, 'dist'),
+        inline: true,    // removes the iframe around the webpage
+        stats: 'errors-only'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({ // npm i -D html-webpack-plugin
+            template: path.join(__dirname, 'src', 'index.html')
+        })
+    ]
 }
