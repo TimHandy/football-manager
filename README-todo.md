0d72547de54319999d52355492433244815c0f6c
Tim Handy <tjhandy@gmail.com>
2017-03-15 08:30:00 +0000

    Add webpack export to docs dir

---
+++ README-todo.md
@@ -0,0 +1,4 @@
+
+Configure webpack-html-plugin:
+http://javascriptplayground.com/blog/2016/07/webpack-html-plugin/
+I think I need to add the inject: 'body' line, so that scripts go in the body, not the head.
--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
 
     // define output points
     output: {
-        path: path.join(__dirname, 'build'),
+        path: path.join(__dirname, 'docs'),
         filename: 'bundle.js'		
     },
     
